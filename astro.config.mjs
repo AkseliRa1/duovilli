11b1f4e19b501dd6d50f4c15f405d4b586426cdb
Akseli Rantola <61066051+isobube@users.noreply.github.com>
2026-09-15 12:43:34 +0300

    Update site configuration and favicon link in Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://akselira1.github.io',
+  base: '/duovilli',
   vite: {
     plugins: [tailwindcss()]
   }
